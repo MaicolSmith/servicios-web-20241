b5136cc6bf7cd9b4ce3be0ac3d10078cf9f8dfd0
MaicolSmith <maicol.smith.bueno@gmail.com>
2024-03-13 21:10:28 -0500

    resultados de operadores

---
+++ comparacion.js
@@ -0,0 +1,4 @@
+let counter ="100"
+let age =100
+let result = counter == age 
+console.log (result)
---
+++ operadores.js
@@ -0,0 +1,23 @@
+let counter = 1000;
+//suma
+counter = counter + 20;//1050
+counter +=30;//1050
+console.log(counter)
+//resta
+counter = counter - 20;//1030
+console.log(counter)
+counter -=30;//1030
+// multiplicacion
+counter = counter *3;//3000
+console.log("linea 12",counter)
+counter *=4;//
+//division
+counter = counter / 5;
+counter /=4;
+//residuo
+counter = counter % 3;
+counter %= 3;
+//exponesacion
+counter = counter **3;
+counter **=2;
+
